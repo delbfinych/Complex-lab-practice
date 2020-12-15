c7be426cfafe4e8e9b626d8ebb2046521a2f2e42
natali <ermolina_natali@bk.ru>
2020-12-15 21:11:08 +0300

    Add function getAbs

--- complex.c
+++ complex.c
@@ -1,1 +1,13 @@
 #include "complex.h"
+
+double getNorm(const ComplexNumber number) {
+	double x = number.real;
+	double y = number.imag;
+	return x*x + y*y;
+}
+
+double getAbs(const ComplexNumber number ) {
+	double x = number.real;
+	double y = number.imag;
+	return sqrt(x*x + y*y);
+}
