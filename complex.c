8b6c828dbe9ad21440598d1eab0e65c0a4be3b27
natali <ermolina_natali@bk.ru>
2020-12-15 22:58:32 +0300

    Add function mult

--- complex.c
+++ complex.c
@@ -9,3 +9,15 @@ double getNorm(const ComplexNumber number) {
 double getAbs(const ComplexNumber number ) {
 	return sqrt(getNorm(number));
 }
+
+ComplexNumber mult(const ComplexNumber first, const ComplexNumber second) {
+	double x1 = first.real;
+	double y1 = first.imag;
+	double x2 = second.real;
+	double y2 = second.imag;
+	ComplexNumber res;
+	res.real = x1*x2 - y1*y2;
+	res.imag = x1*y2 + y1*x2;
+	return res;
+}
+
